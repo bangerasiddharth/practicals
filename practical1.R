681d908c72cde034afc0dccdceb5e088afd996e4
Siddharth Bangera <bangerasiddharth@gmail.com>
2021-05-28 11:09:52 +0100

    Initial commit

--- practical1.R
+++ practical1.R
@@ -1,11 +1,12 @@
-# First step 
-# Assign number 2 to variable x
+# First step
+# Assign number 2 to vaiable x
 x <- 2
 # same as x = 2
 y <- 5
-y 
+y
 rm(y)
 y
+
 class(x)
 # test to see if x is numeric
 is.numeric(x)
@@ -15,14 +16,11 @@ a <- 4L
 class(a)
 b <- 2.8
 class(b)
-c <- a*b
+c <- a * b
 class(c)
 class(5L)
 class(2L)
-class(5L/2L)
-
-
-#factor
+class(5L / 2L)
 
 x <- "data"
 x
@@ -31,23 +29,21 @@ y <- factor("data")
 y
 nchar(x)
 
-#date
-
-date1 <- as.Date("2021-03-11")
+date1 <- as.Date("2021-11-03")
 date1
 class(date1)
 as.numeric(date1)
 
-
 # vectors
-
-p <- c(3,5,6,8)
-q <- c(3,3,3)
+p <- c(3, 5, 6, 8)
+q <- c(3, 3, 3)
 r <- p + q
 r
 
-# vector arithmetic
-#create a vetor y to contain all of x, 0, all of y
+# vector arithmitic
+# create a vector y to contain all of 
+# x, 0, all of x
+# y <- x, 0, x
 x <- c(10.4, 5.6, 3.1, 6.4, 21.7)
 y <- c(x, 0, x)
 y
@@ -59,26 +55,25 @@ max(z)
 range(z)
 mean_of_z <- mean(z)
 mean_of_z
-sortred_z <- sort(z)
-sortred_z
+sorted_z <- sort(z)
+sorted_z
 
-#sequence
 seq3 <- seq(-100, 100, by=0.6)
 seq3
 sort(seq3, decreasing = TRUE)
 
-#practicaldemo
-#vectors
+# Practical demo
+# add contents into vec1
+vec1 <- c("Hockey", "Football", "Baseball", "Curling", "Rugby", 
+          "Hurling", "Basketball", "Tennis", "Cricket", "Lacrosse")
 
-vec1 <- c("Hockey","Football","Baseball","Curling","Rugby", "Hurling", "Basketball", "Tennis", "Cricket", "Lacrosse")
-vec2 <- c(vec1, "Hockey", "Lacrosse", "Hockey", "Water Polo", "Hockey", "Lacrosse")
-
+vec2 <- c(vec1, "Hockey", "Lacrosse", "Hockey", 
+          "Water Polo", "Hockey", "Lacrosse")
+vec2
 vec2_factor <- as.factor(vec2)
 vec2_factor
 
-vec3 <- vec2 [c(1,3,6)]
+# Extract the first, third and sixth element  
+# from vec2 and insert into vec3
+vec3 <- vec2[c(1,3,6)]
 vec3
-
-vectp <- c(vec2[1],vec2[3],vec2[6])
-vectp
-
